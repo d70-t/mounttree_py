837b5c0fefa25ba06b48accca824a46ecc3ec8ae
Tobias Kölling <tobi@die70.de>
2020-06-20 18:48:43 +0200

    removed f-strings for Python 3.5 support
    Python 3.5 is not yet at end of life, so it should still be supported.

--- mounttree/debug.py
+++ mounttree/debug.py
@@ -1,10 +1,10 @@
 def debug(func):
     def debug_wrapper(*args, **kwargs):
         args_repr = [repr(a) for a in args]
-        kwargs_repr = [f"{k}={v!r}" for k, v in kwargs.items()]  # 2
-        signature = ", ".join(args_repr + kwargs_repr)           # 3
-        print(f"Calling {func.__name__}({signature})")
+        kwargs_repr = ["{}={}".format(k, repr(v)) for k, v in kwargs.items()]
+        signature = ", ".join(args_repr + kwargs_repr)
+        print("Calling {}({})".format(func.__name__, signature))
         value = func(*args, **kwargs)
-        print(f"{func.__name__!r} returned {value}")           # 4
+        print("{} returned {}".format(repr(func.__name__), value))
         return value
     return debug_wrapper
